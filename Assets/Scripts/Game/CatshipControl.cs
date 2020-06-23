a8648e874abf879e87e319162457c5305efca486
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-06-22 22:50:02 -0300

    jogo quase pronto

--- Assets/Scripts/Game/CatshipControl.cs
+++ Assets/Scripts/Game/CatshipControl.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class CatshipControl : MonoBehaviour
 {
-    public float rotSpeed, propulsion;
+    public float rotSpeed, propulsion, tolerance;
 
     Transform tr;
     Rigidbody2D rb;
 
     Vector2 dirForce;
+    float rbVel;
 
     void Awake() {
         tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update() {
+    void Start() {
+        //StartCoroutine(ObserveVelocity());
+    }
+
+    void FixedUpdate() {
         Rotate();
         Propulsion();
+        rbVel = rb.velocity.magnitude;
     }
+//
+    //IEnumerator ObserveVelocity() {
+        //while (true) {
+            //yield return WaitForSeconds(0.1f);
+        //}
+    //}
 
     void Rotate() {
         float dirSpeed = Input.GetAxis("Horizontal");
@@ -38,6 +50,17 @@ public class CatshipControl : MonoBehaviour
     }
 
     void OnCollisionEnter2D(Collision2D col) {
+        string tag = col.gameObject.tag;
 
+        Debug.Log(rbVel);
+
+        if (tag == "GroundTilemap")
+            GameState.instance.Perder();
+        else if (tag == "GroundBase") {
+            if (rbVel < tolerance)
+                GameState.instance.Ganhar();
+            else
+                GameState.instance.Perder();
+        }
     }
 }
--- Assets/Scripts/Game/GameState.cs
+++ Assets/Scripts/Game/GameState.cs
@@ -1,18 +1,29 @@
+﻿using System;
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static GameState instance;
+    public GameObject[] fases;
+    Action actPerder;
+
+    void Awake() {
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Perder() {
+        Debug.Log("Perder");
+        if (actPerder != null)
+            actPerder.Invoke();
+    }
+
+    public void Ganhar() {
+        Debug.Log("Ganhar");
+    }
+
+    public void GanharJogo() {
+        Debug.Log("Ganhar Jogo");
     }
 }
